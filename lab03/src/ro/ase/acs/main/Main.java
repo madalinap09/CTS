5c045aac73fe5f1f0f8d446537fffb1189466a57
madalinap09 <madalinaf.pescaru@gmail.com>
2021-03-11 14:57:12 +0200

    Lab 3.1

--- lab03/src/ro/ase/acs/main/Main.java
+++ lab03/src/ro/ase/acs/main/Main.java
@@ -12,12 +12,12 @@ import ro.ase.acs.interfaces.UnaryOperator;
 
 public class Main {
 	
-	public static int transform(int x) {
-		if(x % 2 == 0) {
-			return x / 2;
+	public static int transformNumber(int nr) {
+		if(nr % 2 == 0) {
+			return nr / 2;
 		}
 		else {
-			return 2 * x;
+			return 2 * nr;
 		}
 	}
 
@@ -45,7 +45,10 @@ public class Main {
 		operator = (o1, o2) -> o1 * o2;
 		System.out.println(operator.operate(3, 5));
 		
-		operator = (x, y) -> { double sum = x + y; return sum / 2; };
+		operator = (x, y) -> 
+			{ double sum = x + y; 
+			  return sum / 2; 
+			};
 		
 		Moveable m = () -> System.out.println("The car is moving");
 		m.move();
@@ -57,22 +60,22 @@ public class Main {
 		long nb = list.stream().filter(e -> (e % 2 == 0)).count();
 		System.out.println(nb);
 		
-		List<Integer> newList = list.stream().distinct().
-									filter(e -> e < 8).sorted().collect(Collectors.toList());
+		List<Integer> newList = 
+				list.stream().distinct().filter(e -> e < 8).sorted().collect(Collectors.toList());
 		for(Integer i : newList) {
 			System.out.println(i);
-		}
+			}
 		
 		List<String> stringList = Arrays.asList("something", "something else", "a", "ab", "abc");
-		String result = stringList.stream().
-							filter(s -> s.length() > 2).collect(Collectors.joining(", "));
+		String result = 
+				stringList.stream().filter(s -> s.length() > 2).collect(Collectors.joining(", "));
 		System.out.println(result);
 		
 		list.stream().distinct().sorted().map(e -> e * e).forEach(e -> System.out.println(e));
 		
 		random.ints().limit(5).sorted().forEach(System.out::println);
 		
-		list.stream().map(Main::transform).forEach(System.out::println);
+		list.stream().map(Main::transformNumber).forEach(System.out::println);
 		
 		String filteredList = stringList.parallelStream().
 			filter(e -> e.startsWith("a")).collect(Collectors.joining(", "));
